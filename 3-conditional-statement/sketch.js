c3486f02739f2afaf2ac602e78cefabc8d9ef061
GrahamYater <yaterg@gmail.com>
2019-11-19 13:55:52 -0800

    Logs
    Made logs shape and it bounces.

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -1,5 +1,5 @@
 //create an empty array called balls
-let balls = [];
+let logs = [];
 
 //create a variable to hold your avatar
 let me;
@@ -23,16 +23,16 @@ function draw(){
   me.moveMe();
 
   if (frameCount % 15 == 0) {
-      let  b = new Ball(width, random(0,height), -3, random(-2,2), random(0,255), random(0,255), random(0,255), false);
-      balls.push(b);
-      console.log(balls); //print the balls array to the console
+      let  b = new Log(500, 400, 15, random(-4,4), 97, 37, 19, false);
+      logs.push(b);
+      console.log(logs); //print the balls array to the console
     }
 
 //	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	 	      balls[i].drawBall();
-       	  balls[i].moveBall();
-        	balls[i].bounceBall();
+	for (let i = 0; i < logs.length; i++) {
+	 	      logs[i].drawLog();
+       	  logs[i].moveLog();
+        	logs[i].bounceLog();
 	  }
 
 }
@@ -47,16 +47,7 @@ class Avatar {
 	}
 
 	drawMe(){  // draw the running person
-    		stroke(random(0,255), random(0,255), random(0,255));
-        strokeWeight(3);
-    		fill(random(0,255), random(0,255), random(0,255));
-		    ellipse(this.x,this.y,20,20);
-        line(this.x,this.y, this.x, this.y+40);
-        line(this.x, this.y+40, this.x-20, this.y+60);
-        line(this.x, this.y+40, this.x+10, this.y+50);
-        line(this.x+10, this.y+50, this.x+5, this.y+60);
-        line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
+    		 rect(this.x,this.y,100,30);
 	}
 
 	moveMe(){
@@ -84,7 +75,7 @@ class Avatar {
 
 
 //ball class from which to create new balls with similar properties.
-class Ball {
+class Log {
 
 	//every ball needs an x value, a y value, and a speed
 	constructor(x,y, speed, yspeed, r, g, b, flipped){
@@ -99,22 +90,22 @@ class Ball {
 	}
 
 	// draw a ball on the screen at x,y
-	drawBall(){
+	drawLog(){
     	stroke(0);
       strokeWeight(2);
     	fill(this.r, this.g, this.b);
-		  rect(this.x,this.y,10,20);
+		  rect(this.x,this.y,100,30);
 	}
 
 	//update the location of the ball, so it moves across the screen
-	moveBall(){
+	moveLog(){
 		this.x = this.x+ this.speed;
 		this.y = this.y+this.yspeed;
 	}
 
 	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
-    	 if (this.x >= me.x-75 && this.x <= me.x+75 && this.y > me.y-80 && this.y < me.y+80 && this.flipped == false){
+  	bounceLog(){
+    	 if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-15 && this.y < me.y+15 && this.flipped == false){
       			this.speed = -this.speed;
             this.flipped = true
     		}
